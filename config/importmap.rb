ef608641692f54e74ad2338589cc3e558c91e4c8
Bryan Mendoza Guerrero <bryan.mendoza.michelada@kueski.com>
2024-03-07 20:10:50 -0600

    Configure stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,10 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin '@stimulus/webpack-helpers', to: '@stimulus--webpack-helpers.js' # @2.0.0
+pin '@hotwired/stimulus-webpack-helpers', to: '@hotwired--stimulus-webpack-helpers.js' # @1.0.1
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'application'
+pin 'register'
