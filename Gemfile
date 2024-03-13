57f9be0d392c2741a394aa2055e93f28b0df2415
Bryan Mendoza Guerrero <bryan.mendoza.michelada@kueski.com>
2024-03-12 18:34:55 -0600

    Add index method grouped by category

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
+gem 'httparty'
+gem 'poke-api-v2'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require 'poke-api-v2'
+
 class PokemonsController < ApplicationController
-  before_action :set_pokemon, only: %i[show]
+  before_action :set_response_pokemons_api, only: %i[index]
 
   # GET /pokemons
   def index
-    @pokemons = nil
+    set_pokemons if params[:pokemon_type]
+    @pagination = @pokemons_api.slice(:count, :next, :previous)
+    @pokemon_types = @pokemons_api[:results]
   end
 
   # GET /pokemons/{id}
@@ -13,7 +17,13 @@ class PokemonsController < ApplicationController
 
   private
 
-  def set_pokemon
-    @pokemon = '' # WIP: Comes from API
+  def set_response_pokemons_api
+    # Leave as HTTP request instead of PokeAPi gem to show an example of how it works
+    # This can be replaced by: PokeApi.get(pokemon_shape: {limit: 5, offset: 5})
+    @pokemons_api = JSON.parse(HTTParty.get('https://pokeapi.co/api/v2/type').body, symbolize_names: true)
+  end
+
+  def set_pokemons
+    @pokemons = PokeApi.get(type: params[:pokemon_type])
   end
 end
